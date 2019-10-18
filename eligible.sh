00bbd58a77221c98c468b25a42c09edec92df48b
Philippe Jean Guillaumie <similar@orange.fr>
2019-10-18 08:39:06 +0200

    Remove autopoint from the dependencies

--- eligible.sh
+++ eligible.sh
@@ -66,8 +66,8 @@ RELEASE=$(lsb_release -sc)
 ICNV=libiconv-1.16
 
 # Build dependencies, recommended(2) and script-related(3) packages.
-DEPS="aspell automake autopoint build-essential ccache check cmake cowsay \
-doxygen faenza-icon-theme git gstreamer1.0-libav gstreamer1.0-plugins-bad \
+DEPS="aspell automake build-essential ccache check cmake cowsay doxygen \
+faenza-icon-theme git gstreamer1.0-libav gstreamer1.0-plugins-bad \
 gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly imagemagick \
 libasound2-dev libavahi-client-dev libblkid-dev libbluetooth-dev \
 libbullet-dev libcogl-gles2-dev libexif-dev libfontconfig1-dev \
