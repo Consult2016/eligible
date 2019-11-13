99753b8760fdf2648667967bb50278a5f85c37bc
Philippe Jean Guillaumie <similar@orange.fr>
2019-11-13 12:47:32 +0100

    Refactor function rebuild_wld

--- eligible.sh
+++ eligible.sh
@@ -702,9 +702,12 @@ wld_go() {
   rebuild_wld
 
   sudo mkdir -p /etc/enlightenment
+  sudo mkdir -p /usr/share/wayland-sessions
 
   sudo ln -sf /usr/local/etc/enlightenment/sysactions.conf /etc/enlightenment/sysactions.conf
   sudo ln -sf /usr/local/etc/xdg/menus/e-applications.menu /etc/xdg/menus/e-applications.menu
+  sudo ln -sf /usr/local/share/wayland-sessions/enlightenment.desktop \
+    /usr/share/wayland-sessions/enlightenment.desktop
 
   sudo updatedb
   beep_ok
