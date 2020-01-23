0b3984107763d267eb85c937d4db4801787dbe23
Philippe Jean Guillaumie <similar@orange.fr>
2020-01-23 13:54:36 +0100

    Minor correction

--- eligible.sh
+++ eligible.sh
@@ -182,7 +182,7 @@ bin_deps() {
 }
 
 ls_dir() {
-  COUNT=$(ls -d */ | wc -l)
+  COUNT=$(ls -d -- */ | wc -l)
   if [ $COUNT == 3 ]; then
     printf "$BDG%s $OFF%s\n\n" "All programs have been downloaded successfully."
     sleep 2
